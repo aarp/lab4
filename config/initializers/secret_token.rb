a433b6be886fdc9ce5615be4c753d2c98d697836
Alberto Ruiz - 20012696 <aarp@galileo.edu>
2011-03-30 16:27:31 -0500

    1er commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lab4::Application.config.secret_token = 'c175144e01ec8d8eb454f481a5e43ab9acb15a6899d5f86bb83798c0c7e695425935b55f54fdac349dc37045c589bae84f312641bb65120bc0353686a05e62a5'
